3528fde377a25292360bb55f016a5de354dd4fda
Trungpham2k4 <trungpdb2k4@gmail.com>
2025-05-29 23:21:28 +0700

    Complete basic backend

---
+++ src/controllers/CommentPageController.js
@@ -0,0 +1,31 @@
+const pool = require("../models/db");
+
+exports.addComment = async (req, res) => {
+  try {
+    const { text, rating } = req.body;
+    const customer_id = req.user.id; // Assuming user ID is stored in req.user.id after authentication
+    // console.log(req.body)
+    await pool.query(`
+        INSERT INTO REVIEWS (CUSTOMER_ID, TOUR_ID, comment, rating)
+        VALUES (?, ?, ?, ?)`,
+        [customer_id, 1, text, rating]
+    );
+    res.status(201).json({ message: "Comment added successfully" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+exports.getComments = async (req, res) => {
+  try {
+    const tourId = req.params.tourId;
+    const [rows] = await pool.query(`
+        SELECT * FROM REVIEWS`
+    );
+    res.json(rows);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 const homepageController = require("../controllers/HomepageController");
+const searchPageController = require("../controllers/SearchPageController");   
+const commentPageController = require("../controllers/CommentPageController"); 
+const authController = require("../controllers/AuthController");
+const tourDetailPageController = require("../controllers/TourDetailPageController");
+
+const middleware = require("../middlewares/authMiddleware");
 
 router.get("/getAllHeritage", homepageController.getAllHeritage);
+router.post("/searchTour", searchPageController.searchTour);
+router.post("/addComment", middleware, commentPageController.addComment);
+router.get("/getComments",commentPageController.getComments);
+router.post("/searchHeritage", tourDetailPageController.getHeritageDetail);
+
+router.post("/signup", authController.signUp);
+router.post("/signin", authController.signIn);
 
 module.exports = router;
